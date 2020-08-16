928e37092c753bfece5ca9ffad3049dadf2a2f96
Owen <owencross99@gmail.com>
2020-08-16 20:16:07 +0100

    First commit

---
+++ MemoryMatcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryMatcher
+//
+//  Created by Owen on 16/08/2020.
+//  Copyright © 2020 Owen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I have changed this text to push!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
