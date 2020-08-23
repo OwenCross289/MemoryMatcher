1e0ee5191b90bd84827277103c8113a14a224c83
Owen <owencross99@gmail.com>
2020-08-23 13:57:38 +0100

    Moved to a MVVM folder layout

---
+++ MemoryMatcher/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  MemoryMatcher
+//
+//  Created by Owen on 23/08/2020.
+//  Copyright © 2020 Owen. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
